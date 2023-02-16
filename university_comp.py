b5167830fcb1a1de734d08e7c97055a603ea3a90
joechou-f26 <71837456+joechou-f26@users.noreply.github.com>
2023-02-16 13:38:07 +0800

    Update university_comp.py

--- university_comp.py
+++ university_comp.py
@@ -28,7 +28,7 @@ st.sidebar.text("台中地區的大學人數變化比較(101-111)")
 with st.sidebar:
     options = st.sidebar.multiselect('選擇想要比較的學校：',
         ['逢甲大學', '東海大學', '靜宜大學','亞洲大學',
-         '僑光科技大學','嶺東科技大學','朝陽科技大學','中臺科技大學','弘光科技大學','修平學校財團法人修平科技大學',
+         '僑光科技大學','嶺東科技大學','朝陽科技大學','中臺科技大學','弘光科技大學',
          '中國醫藥大學','中山醫學大學',
         '國立中興大學','國立臺中教育大學','國立臺中科技大學','國立勤益科技大學','國立臺灣體育運動大學'],
         ['逢甲大學', '東海大學', '靜宜大學'])
@@ -44,7 +44,7 @@ xpos = np.arange(len(year))  #將一個指定範圍的值平均分配，然後�
 for s in schools:
     df3=df2[df2.school_name==s]
     plt.plot(xpos, df3.total, 'o-', label=s)
-plt.title('台中地區大學人數變化比較 (106-111學年)',fontproperties=myFont)
+plt.title('台中地區大學人數變化比較 (106-111學年)',fontproperties=myFont, fontsize='x-large')
 plt.xlabel('學年度',fontproperties=myFont)
 plt.ylabel('學生人數',fontproperties=myFont)
 # plot new xticks and yticks
