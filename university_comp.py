62f05715a46609169268399fe5fbfa2b8017fc69
joechou-f26 <71837456+joechou-f26@users.noreply.github.com>
2023-02-16 14:07:40 +0800

    Update university_comp.py

--- university_comp.py
+++ university_comp.py
@@ -38,13 +38,13 @@ df2=df1.groupby(['year','school_name'])['total'].sum().reset_index()
 schools=df2.sort_values(by='total', ascending=False)['school_name'].unique().tolist()
 
 #--- plot
-fig = plt.figure(figsize=(12,6))  # set up size of figure
+fig = plt.figure(figsize=(12,8))  # set up size of figure
 year=df1['year'].unique()
 xpos = np.arange(len(year))  #將一個指定範圍的值平均分配，然後傳回array
 for s in schools:
     df3=df2[df2.school_name==s]
     plt.plot(xpos, df3.total, 'o-', label=s)
-plt.title('台中地區大學人數變化比較 (106-111學年)',fontproperties=myFont, fontsize='x-large')
+plt.title('台中地區大學人數變化比較 (101-111學年)',fontproperties=myFont, fontsize='x-large')
 plt.xlabel('學年度',fontproperties=myFont)
 plt.ylabel('學生人數',fontproperties=myFont)
 # plot new xticks and yticks
